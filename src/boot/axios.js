9f9d92adff5d266a4d110d0d999364ae2261630f
davidhaw <david@kabaddle.com>
2020-09-09 16:36:37 +0100

    config clean

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -13,7 +13,8 @@ const axiosInstance  = axios.create({
 
 // for use inside Vue files through this.$axios
 Vue.prototype.$axios = axiosInstance
-
+Vue.prototype.$axios.defaults.withCredentials = true;
+Vue.prototype.$axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 // Here we define a named export
 // that we can later use inside .js files:
 export { axiosInstance }
